26655605d640da0b3c02a52e7358bb7ac4a02df8
Miguel Bogota <miguelbogota.rico@outlook.com>
2020-08-05 12:50:11 -0500

    Inicio de sesión y crear cuenta creados
    Se crearon las vistas y funcionalidades del CRUD del usuario en el inicion de sesion y creacion de cuenta. No funciona correctamente ya que se tiene que modificar el API.

--- front-end/src/app/views/signin/signin.component.ts
+++ front-end/src/app/views/signin/signin.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
+import { AuthService } from 'src/app/core/services/auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signin',
@@ -7,9 +10,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SigninComponent implements OnInit {
 
-  constructor() { }
+  signInForm: FormGroup = null;
+  isLoading = false;
+  errorMessage = null;
+
+  constructor(
+    private fb: FormBuilder,
+    private auth: AuthService,
+    private router: Router
+  ) {
+    this.signInForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
 
   ngOnInit(): void {
   }
+
+  formSubmit(): void {
+    this.isLoading = true;
+    this.auth.signIn(this.username.value, this.password.value)
+      .subscribe(u => {
+        if (u) {
+          this.router.navigate([`/profile/${this.username.value}`]);
+          this.isLoading = false;
+        }
+        else {
+          this.errorMessage = 'Las credenciales no son correctas, por favor verificalas.';
+        }
+      });
+  }
+
+  // Getters
+  get username(): AbstractControl { return this.signInForm.get('username'); }
+  get password(): AbstractControl { return this.signInForm.get('password'); }
 
 }
--- front-end/src/app/views/signup/signup.component.ts
+++ front-end/src/app/views/signup/signup.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
+import { AuthService } from 'src/app/core/services/auth/auth.service';
+import { UserService } from 'src/app/core/services/user/user.service';
+import { Router } from '@angular/router';
+import { UserInterface } from 'src/app/core/models/user.model';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +12,67 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  signUpForm: FormGroup;
+  errorMessage: string = null;
+  isLoading = false;
+
+  constructor(
+    private fb: FormBuilder,
+    private auth: AuthService,
+    private userS: UserService,
+    private router: Router
+  ) {
+    this.signUpForm = this.fb.group({
+      username: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      passwordRepeat: ['', Validators.required],
+      displayName: ['', Validators.required]
+    });
+  }
 
   ngOnInit(): void {
   }
+
+  formSubmit() {
+    this.isLoading = true;
+    if (this.password.value === this.passwordRepeat.value) {
+
+      this.userS.getUser(this.username.value).subscribe(u => {
+        if (!u.username) {
+          const user: UserInterface = {
+            email: this.email.value,
+            password: this.password.value,
+            photoUrl: '',
+            status: '¡Hola! ¡Soy nuevo!',
+            subjectIds: [],
+            type: 'student',
+            username: this.username.value,
+            displayName: this.displayName.value
+          }
+          this.auth.signUp(user)
+            .subscribe(u => {
+              this.router.navigate(['/signin']);
+              this.isLoading = false;
+            });
+        }
+        else {
+          this.errorMessage = 'El nombre de usuario ya existe, escoge otro por favor.';
+          this.isLoading = false;
+        }
+      })
+    }
+    else {
+      this.errorMessage = 'Las contraseñas no coinciden, verifica que esten correctas por favor.';
+      this.isLoading = false;
+    }
+  }
+
+  // Getters
+  get username(): AbstractControl { return this.signUpForm.get('username'); }
+  get email(): AbstractControl { return this.signUpForm.get('email'); }
+  get password(): AbstractControl { return this.signUpForm.get('password'); }
+  get passwordRepeat(): AbstractControl { return this.signUpForm.get('passwordRepeat'); }
+  get displayName(): AbstractControl { return this.signUpForm.get('displayName'); }
 
 }
