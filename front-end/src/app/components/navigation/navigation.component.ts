51a2124d8c708e7ea7115cd2ff8058244542a5a1
Miguel Bogota <miguelbogota.rico@outlook.com>
2020-08-05 12:14:09 -0500

    Pagina principar con barra de navegacion creada y funcionando
    Se edito el componente de la barra de navegacion y tambien de la pagina principal.

--- front-end/src/app/components/navigation/navigation.component.ts
+++ front-end/src/app/components/navigation/navigation.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../core/services/auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navigation',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationComponent implements OnInit {
 
-  constructor() { }
+  authState = null;
+
+  constructor(
+    private auth: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.auth.authStateChanges
+      .subscribe(u => {
+        this.authState = u;
+      });
+  }
+
+  signOut() {
+    this.auth.signOut();
+    this.router.navigate(['/']);
   }
 
 }
